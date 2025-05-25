c16a64c01412d6e9cba89b7fd4a70c740a1bcd77
Miguel Nischor <miguel@nischor.com.br>
2025-05-24 20:33:19 -0400

    docs: Adição do comentário de cabeçalho e comentários de variáveis e funções Composable

--- app/src/main/java/br/edu/senai/cac/MainActivity.kt
+++ app/src/main/java/br/edu/senai/cac/MainActivity.kt
@@ -1,3 +1,10 @@
+/*
+* Projeto: CAC - Cadê a Chave?
+* Arquivo: /app/src/main/java/br/edu/senai/cac/MainActivity.kt
+* Descrição: Activity principal do aplicativo.
+* Autor: Miguel Nischor <miguel@docente.senai.br>
+* Data: 24/05/2025
+*/
 package br.edu.senai.cac
 
 import android.os.Bundle
@@ -9,6 +16,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import br.edu.senai.cac.ui.theme.CACTheme
 
+/**
+ * Atividade principal do aplicativo.
+ * Esta atividade é responsável por configurar a interface do usuário e gerenciar o ciclo de vida do aplicativo.
+ * @constructor Cria uma nova instância da atividade principal.
+ * @see [ComponentActivity]
+ * @see [CACTheme]
+ * @author Miguel Nischor <miguel@docente.senai.br>
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
