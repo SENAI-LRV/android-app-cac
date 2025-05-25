b5a1e6f0a381dd7d7678312c95af1fcac104df7e
Miguel Nischor <miguel@nischor.com.br>
2025-05-25 19:47:33 -0400

    code: Repositório inicial para acesso ao cadastro de salas

---
+++ app/src/main/java/br/edu/senai/cac/data/repository/RoomRepository.kt
@@ -0,0 +1,39 @@
+/*
+* Projeto: CAC - Cadê a Chave?
+* Arquivo: /app/src/main/java/br/edu/senai/cac/data/repository/RoomRepository.kt
+* Descrição: Repositório para a entidade RoomModel.
+* Autor: Miguel Nischor <miguel@docente.senai.br>
+* Data: 25/05/2025
+*/
+package br.edu.senai.cac.data.repository
+
+import br.edu.senai.cac.data.dao.RoomDao
+import br.edu.senai.cac.data.models.RoomModel
+import kotlinx.coroutines.flow.Flow
+
+class RoomRepository(private val roomDao: RoomDao) {
+
+    suspend fun insert(room: RoomModel) {
+        roomDao.insert(room)
+    }
+
+    suspend fun update(room: RoomModel) {
+        roomDao.update(room)
+    }
+
+    suspend fun delete(room: RoomModel) {
+        roomDao.delete(room)
+    }
+
+    suspend fun getRoomById(id: String): RoomModel? {
+        return roomDao.getById(id)
+    }
+
+    fun getAllRooms(): Flow<List<RoomModel>> {
+        return roomDao.getAll()
+    }
+
+    suspend fun deleteAllRooms() {
+        roomDao.deleteAll()
+    }
+}
