196c3e1b25f5d7ae31fd78c7fc7401dcb0593267
Miguel Nischor <miguel@nischor.com.br>
2025-05-25 17:23:31 -0400

    code: Alteração do estilo de fonte para negrito

--- app/src/main/java/br/edu/senai/cac/ui/HomeScreen.kt
+++ app/src/main/java/br/edu/senai/cac/ui/HomeScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -47,7 +48,8 @@ fun HomeScreen(
                 top = 16.dp,
                 start = 16.dp,
                 end = 16.dp
-            )
+            ),
+            fontWeight = FontWeight.Bold
         )
 
         OutlinedTextField(
@@ -68,7 +70,8 @@ fun HomeScreen(
                 top = 16.dp,
                 start = 16.dp,
                 end = 16.dp
-            )
+            ),
+            fontWeight = FontWeight.Bold
         )
 
         OutlinedTextField(
