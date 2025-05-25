acc1318b4549fe39f0c3f6830096e3bdb90a4e20
Miguel Nischor <miguel@nischor.com.br>
2025-05-25 19:47:28 -0400

    code: Repositório inicial para acesso ao cadastro de chaves

---
+++ app/src/main/java/br/edu/senai/cac/data/repository/KeyRepository.kt
@@ -0,0 +1,39 @@
+/*
+* Projeto: CAC - Cadê a Chave?
+* Arquivo: /app/src/main/java/br/edu/senai/cac/data/repository/KeyRepository.kt
+* Descrição: Repositório para a entidade KeyModel.
+* Autor: Miguel Nischor <miguel@docente.senai.br>
+* Data: 25/05/2025
+*/
+package br.edu.senai.cac.data.repository
+
+import br.edu.senai.cac.data.dao.KeyDao
+import br.edu.senai.cac.data.models.KeyModel
+import kotlinx.coroutines.flow.Flow
+
+class KeyRepository(private val keyDao: KeyDao) {
+
+    suspend fun insert(key: KeyModel) {
+        keyDao.insert(key)
+    }
+
+    suspend fun update(key: KeyModel) {
+        keyDao.update(key)
+    }
+
+    suspend fun delete(key: KeyModel) {
+        keyDao.delete(key)
+    }
+
+    suspend fun getKeyById(id: String): KeyModel? {
+        return keyDao.getById(id)
+    }
+
+    fun getAllKeys(): Flow<List<KeyModel>> {
+        return keyDao.getAll()
+    }
+
+    suspend fun deleteAllKeys() {
+        keyDao.deleteAll()
+    }
+}
