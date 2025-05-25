8e08c71995dde83f239f8127be8ccfc6e860bd8b
Miguel Nischor <miguel@nischor.com.br>
2025-05-24 22:41:03 -0400

    code: Alteração do layout para a tela de login

--- app/src/main/java/br/edu/senai/cac/ui/HomeScreen.kt
+++ app/src/main/java/br/edu/senai/cac/ui/HomeScreen.kt
@@ -9,12 +9,19 @@
 
 package br.edu.senai.cac.ui
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 /**
  * Tela inicial do aplicativo.
@@ -23,7 +30,49 @@ import androidx.compose.ui.tooling.preview.Preview
  */
 @Composable
 fun HomeScreen(modifier: Modifier) {
-    Box(modifier = Modifier.fillMaxSize())
+    Column(verticalArrangement = Arrangement.Center,
+           modifier = Modifier.fillMaxSize()) {
+
+        Text(text = "Usuário:",
+             modifier = Modifier.padding(top = 16.dp,
+                                         start = 16.dp,
+                                         end = 16.dp))
+
+        OutlinedTextField(value = "",
+                          onValueChange = {},
+                          modifier = Modifier.padding(top = 8.dp,
+                                                      start = 16.dp,
+                                                      end = 16.dp)
+                                             .fillMaxWidth())
+
+        Text(text = "Senha:",
+             modifier = Modifier.padding(top = 16.dp,
+                                         start = 16.dp,
+                                         end = 16.dp))
+
+        OutlinedTextField(value = "",
+                          onValueChange = {},
+                          modifier = Modifier.padding(top = 8.dp,
+                                                      start = 16.dp,
+                                                      end = 16.dp)
+                                             .fillMaxWidth())
+
+        Button(modifier = Modifier.padding(top = 16.dp,
+                                           start = 16.dp,
+                                           end = 16.dp)
+               .fillMaxWidth(),
+               onClick = {}) {
+            Text(text = "Acessar")
+        }
+
+        Button(modifier = Modifier.padding(top = 8.dp,
+                                           start = 16.dp,
+                                           end = 16.dp)
+               .fillMaxWidth(),
+               onClick = {}) {
+            Text(text = "Esqueci minha senha")
+        }
+    }
 }
 
 /**
