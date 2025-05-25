b9c92579ae078e6d93a41157f82b59064b4b8a4e
Miguel Nischor <miguel@nischor.com.br>
2025-05-25 17:24:14 -0400

    code: Adição da fonte Fira Sans na tipografia

--- app/src/main/java/br/edu/senai/cac/ui/theme/Type.kt
+++ app/src/main/java/br/edu/senai/cac/ui/theme/Type.kt
@@ -9,13 +9,21 @@ package br.edu.senai.cac.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.edu.senai.cac.R
+
+val firaSansFamily = FontFamily(
+    Font(R.font.fira_sans, FontWeight.Normal),
+    Font(R.font.fira_sans_bold, FontWeight.Bold),
+    Font(R.font.fira_sans_light, FontWeight.Light)
+)
 
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = firaSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -23,7 +31,7 @@ val Typography = Typography(
     ),
 
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = firaSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
@@ -31,7 +39,7 @@ val Typography = Typography(
     ),
 
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = firaSansFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
