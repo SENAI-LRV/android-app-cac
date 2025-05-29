bdacc3ed8e20b984ad047dc27a6014ce953f69aa
Miguel Nischor <miguel@nischor.com.br>
2025-05-28 23:00:48 -0400

    code: Remoção da inicialização do banco para utilização em outro arquivo

--- app/src/main/java/br/edu/senai/cac/data/database/AppDatabase.kt
+++ app/src/main/java/br/edu/senai/cac/data/database/AppDatabase.kt
@@ -7,9 +7,7 @@
 */
 package br.edu.senai.cac.data.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import br.edu.senai.cac.data.dao.KeyDao
 import br.edu.senai.cac.data.dao.RoomDao
@@ -18,31 +16,9 @@ import br.edu.senai.cac.data.models.KeyModel
 import br.edu.senai.cac.data.models.RoomModel
 import br.edu.senai.cac.data.models.TeacherModel
 
-@Database(
-    entities = [KeyModel::class, RoomModel::class, TeacherModel::class],
-    version = 1,
-    exportSchema = false
-)
+@Database(entities = [KeyModel::class, RoomModel::class, TeacherModel::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-
     abstract fun keyDao(): KeyDao
     abstract fun roomDao(): RoomDao
     abstract fun teacherDao(): TeacherDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "cac_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
