efe98180e78dc434af111093b89804a1aa28bc8b
Miguel Nischor <miguel@nischor.com.br>
2025-05-28 23:24:05 -0400

    code: Adição de comentários de classe

--- app/src/main/java/br/edu/senai/cac/data/repository/RoomRepository.kt
+++ app/src/main/java/br/edu/senai/cac/data/repository/RoomRepository.kt
@@ -12,6 +12,12 @@ import br.edu.senai.cac.data.models.RoomModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Repositório para a entidade RoomModel.
+ * Fornece métodos para inserir, atualizar, deletar e consultar salas.
+ * @property roomDao Acesso ao DAO para operações de banco de dados.
+ * @author Miguel Nischor <miguel@docente.senai.br>
+ */
 class RoomRepository @Inject constructor(
     private val roomDao: RoomDao
 ) {
