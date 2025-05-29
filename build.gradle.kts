a6de88da616a58edae0f000300f45ae7c2515208
Miguel Nischor <miguel@nischor.com.br>
2025-05-28 22:58:27 -0400

    deps: Adição do plugin do Hilt para o Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.hilt.gradle.plugin) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.kapt) apply false
