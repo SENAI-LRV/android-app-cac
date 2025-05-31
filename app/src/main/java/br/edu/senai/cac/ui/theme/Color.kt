7a3c4a1d8a6cd55a0beb9a4cb3a4fdf819e20a9a
Miguel Nischor <miguel@nischor.com.br>
2025-05-30 21:26:32 -0400

    code: Melhorias na paleta de cores utilizada para tons mais profissionais

--- app/src/main/java/br/edu/senai/cac/ui/theme/Color.kt
+++ app/src/main/java/br/edu/senai/cac/ui/theme/Color.kt
@@ -9,10 +9,25 @@ package br.edu.senai.cac.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Tema escuro
+val ProDarkPrimary = Color(0xFF79B8FF)
+val ProDarkSecondary = Color(0xFFB0C4DE)
+val ProDarkTertiary = Color(0xFF89D194)
+val ProDarkOnPrimary = Color(0xFF003062)
+val ProDarkOnSecondary = Color(0xFF253140)
+val ProDarkOnTertiary = Color(0xFF00391C)
+val ProDarkBackground = Color(0xFF1A1C1E)
+val ProDarkSurface = Color(0xFF1A1C1E)
+val ProDarkOnBackground = Color(0xFFE2E2E6)
+val ProDarkOnSurface = Color(0xFFE2E2E6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Tema claro
+val ProLightPrimary = Color(0xFF0061A4)
+val ProLightSecondary = Color(0xFF526070)
+val ProLightOnPrimary = Color.White
+val ProLightOnSecondary = Color.White
+val ProLightOnTertiary = Color.White
+val ProLightBackground = Color(0xFFFDFBFF)
+val ProLightSurface = Color(0xFFFDFBFF)
+val ProLightOnBackground = Color(0xFF1A1C1E)
+val ProLightOnSurface = Color(0xFF1A1C1E)
